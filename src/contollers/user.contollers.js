803d497470a307edd8f1228df90060bfc5e14efc
mantu kumar <mantukumarverma2004@gmail.com>
2023-12-11 09:38:56 +0530

    jwt update

--- src/contollers/user.contollers.js
+++ src/contollers/user.contollers.js
@@ -5,15 +5,6 @@ import { uploadOnCloudinary } from "../utils/cloudnary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    // get user details from frontend
-    // validation - not empty
-    // check if user already exists: username, email
-    // check for images, check for avatar
-    // upload them to cloudinary, avatar
-    // create user object - create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
-    // return res
 
 
     const { fullName, email, username, password } = req.body
